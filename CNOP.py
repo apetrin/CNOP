c1b97d8ef6793f7964c176da5249eb647f543e3a
apetrin <apetrin@nes.ru>
2015-01-05 23:08:55 +0300

    initial fit
    The first (untested) version of fit
    Input refactoring to dict-type

--- CNOP.py
+++ CNOP.py
@@ -3,27 +3,42 @@ import statsmodels.api as sm
 from scipy import stats
 import numpy as np 
 from statsmodels.sandbox.distributions.extras import mvstdnormcdf
+from scipy.optimize import minimize
+from itertools import izip
+from scipy.optimize import minimize
 
-
-#def jac(x, i):
-#    jac = np.zeros(len(x))
-#    jac[i+1], jac[i]  = -1, 1
-#    return jac
+FLOAT_EPS = np.finfo(float).eps
 
 class CNOP(st.discrete.discrete_model.DiscreteModel):
     """Correlatesdf BLA BLA BLA
     
     Description:
     TO BE WRITTEN"""
+
     def __init__(self,endog, exog, **kwargs):
-        self.J, self.x, self.zplus, self.zminus = endog
+        #endog is a dict of endog.vars
+        self.x, self.zplus, self.zminus = map(endog.get, ['x', 'zplus', 'zminus'])
         self.y = exog
-    
+        self.interest_step = endog.get('interest_step', 0.000125)
+        if "J" in endog:
+            self.J = endog.get("J")
+        else:        
+            self.J = max(self.x.abs().max().T["Y"]) / self.interest_step
+        try:
+            self.param_len = int(2*self.J - 2 + len(self.x.minor_axis) + 
+                                len(self.zminus.minor_axis) + len(self.zplus.minor_axis))
+            self.alpha_len, self.beta_len = 2, len(self.x.minor_axis)-2
+            self.mum_len, self.gammam_len = self.J, len(self.zminus.minor_axis)-2
+            self.mup_len, self.gammap_len = self.J, len(self.zplus.minor_axis)-2
+            self.rhom, self.rhop = 1, 1
+        except AttributeError:
+            pass
+
     informcode = {0: 'normal completion with ERROR < EPS',
                   1: '''completion with ERROR > EPS and MAXPTS function values used;
                         increase MAXPTS to decrease ERROR;''',
                   2: 'N > 500 or N < 1'}
-    
+
     def cdf(self, X, Y=None, rho=None):
         """ Returns 1D or 2D standard normal CDF"""
         if Y is None: return stats.norm._cdf(X)
@@ -34,9 +49,44 @@ class CNOP(st.discrete.discrete_model.DiscreteModel):
             print('something wrong', self.informcode[inform], error)
         return value
 
+    def tester(self):
+        x = [14.1,18.2,12.1,23.1,-12,123]
+        J=4
+        print self.cdf(0,0,1)
+        test = self.cons_generator([(0,4)])
+        cons = eval(test)
+        #print cons[1]
+        #print self.J
+
+    #def jac(x, i):
+    #    jac = np.zeros(len(x))
+    #    jac[i+1], jac[i]  = -1, 1
+    #    return jac
+
+    def cons_generator(self, slice, type = "ineq"):
+        """#indian
+        Function generates a string of constrants,
+        required for optimization routine in scipy.optimize.
+
+        You only need to eval() the output.
+
+        INPUT: list of slices, e.g. [(2,4),(7,12),(14,19)]
+        constrants are set in between slices
+        """
+        constrsstr = "["
+        for st, fin in slice:
+            for i in range(st,fin-1):
+                #constrsstr += '{"type":"' +type + '","fun":lambda x:np.array([float(x[' + \
+                #        str(i+1) + ']-x[' + str(i) + '])]),' +\
+                #        '"jac":lambda x:jac(x, '+str(i)+')},'
+                constrsstr += '{"type":"%s","fun":lambda x:np.array([float(x[%s]-x[%s])])},' \
+                                %(type, str(i+1), str(i))
+
+        constrsstr += "]"
+        return constrsstr
+
     def loglike(self, params):
         """
-        NOT YET TESTED 20150104!!!!!!!!!!!!!!!!!!!!!!!
         Log-likelihood of CNOP model.
         params -- [alpha, beta, mu-, gamma-, mu+, gamma+, rho+, rho-]
         len(alpha) = 2
@@ -47,7 +97,8 @@ class CNOP(st.discrete.discrete_model.DiscreteModel):
         len(gamma+) = len(self.zplus.minor_axis)-2
         len(rho+) = 1
         len(rho-) = 1
-        I use COBYLA algorithm
+        I use     def fit(self, start_params=None, method='newton', maxiter=35,
+            full_output=1, disp=1, callback=None, **kwargs): algorithm
         
         np.dot(pan[1].loc[3.1998],params)
         """
@@ -61,16 +112,17 @@ class CNOP(st.discrete.discrete_model.DiscreteModel):
         rhop, rhom = params
         del params[:2]
         assert len(params) is 0, "params isn't empty!"
-        
+
         s = 0
         x = self.x
         zm = self.zm
         zp = self.zp
-        j = round(xelement["Y"],10)
+        j = int(round(xelement["Y"],10) / interest_step )
         for (xitem, xdf),   (zmitem, zmdf), (zpitem, zpdf) in \
         izip(x.iteritems(), zm.iteritems(), zp.iteritems() ): 
             for (xtime, xelement), (zmtime, zmelement), (zptime, zpelement) in \
             izip(xdf.iterrows(),   zmdf.iterrows(),     zpdf.iterrows() ): 
+                #Two Sums Here,
                 #xelement, zmelement, zpelement are Series of interest for item xitem
                 # and for time xitem. Items and times are identical throughout theree Panels
                 assert xitem == zmitem, "Items doesn't match: xitem != zmitem"
@@ -80,55 +132,28 @@ class CNOP(st.discrete.discrete_model.DiscreteModel):
 
                 if j == 0:
                     pr = self.cdf(alpha[1] - np.dot(xelement, beta)) - self.cdf(alpha[0] - np.dot(xelement, beta))
-                if j <= 0:
+                if j >= 0:
                     pr =  self.cdf(np.dot(xelement, beta) - alpha[1], mup[abs(j)-1]-np.dot(zpelement, gammap), -rhop)
                     pr -= self.cdf(np.dot(xelement, beta) - alpha[1], mup[abs(j)-2]-np.dot(zpelement, gammap), -rhop)
-                if j >= 0:
+                if j <= 0:
                     pr =  self.cdf(alpha[0] - np.dot(xelement, beta), mum[abs(j)-1]-np.dot(zmelement, gammam), rhom)
                     pr -= self.cdf(alpha[0] - np.dot(xelement, beta), mum[abs(j)-2]-np.dot(zmelement, gammam), rhom)
-                pr = np.clip(pr, FLOAT_EPS, 1)
-                s += np.log(pr)
+                s += np.log(np.clip(pr, FLOAT_EPS, 1))
         return s
 
+    def fit(self, start_params=None, method='COBYLA', maxiter=35,
+            full_output=1, disp=1, callback=None, **kwargs):
+        """method are COBYLA and SLSQP [DEPRECIATED ADD JAC!]. ___Subject to check, COBYLA is better on simple tasks"""
+        if start_params is None: start_params = np.zeros(self.param_len)
+        constraints = eval(self.cons_generator([(self.alpha_len + self.beta_len, self.alpha_len + self.beta_len + self.J),
+                                      (self.alpha_len + self.beta_len + self.J + self.gammam_len,
+                                       self.alpha_len + self.beta_len + self.J + self.gammam_len + self.J)
+                                      ]))
+        return minimize(self.loglike, x0=start_params, method=method, constraints=constraints,
+                        options = {'maxiter':maxiter, 'disp':disp}, callback=callback
+                        )
 
-    def tester(self):
-        x = [14.1,18.2,12.1,23.1,-12,123]
-        J=4
-        print self.cdf(1,0,1)
-        test = self.cons_generator(J)
-        cons = eval(test)
-        print cons[1]
-        #print self.J
 
-    def jac(x, i):
-        jac = np.zeros(len(x))
-        jac[i+1], jac[i]  = -1, 1
-        return jac
-    
-    def cons_generator(self, J, type = "ineq"):
-        """#indian
-        Function generates a string of constrants,
-        required for optimization routine in scipy.optimize.
-        
-        You only need to eval() the output.
-        """
-        constrsstr = "["
-        for i in range(J-1):
-            constrsstr += '{"type":"' +type + '","fun":lambda x:np.array([float(x[' + \
-                    str(i+1) + ']-x[' + str(i) + '])]),' +\
-                    '"jac":lambda x:jac(x, '+str(i)+')},'
-        constrsstr += "]"
-        return constrsstr
-        
-    
-    
-TEST = CNOP([1,[12,13],[14,15],[17,17]],[12.15, 12.30])
+
+TEST = CNOP({'x':[12,13], 'zplus':[14,15], 'zminus': [17,17], 'J':2},[12.15, 12.30])
 TEST.tester()
-
-x = [14.1,18.2,12.1,23.1,-12,123]
-J=4
-
-#test = TEST.cons_generator(J)
-#cons = eval(test)
-#print cons
-
